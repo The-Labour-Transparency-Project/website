a0e50998976411a4373aeb4611b30817c867a4eb
todd <todd+github@goneopen.com>
2026-07-27 11:47:50 +1200

    feat: base content

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
+import type { EnhanceAppContext } from 'vitepress'
 import './style.css'
 import ArticleInfographic from './components/ArticleInfographic.vue'
 import LibraryInfographicGallery from './components/LibraryInfographicGallery.vue'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
+  enhanceApp({ app }: EnhanceAppContext) {
     app.component('ArticleInfographic', ArticleInfographic)
     app.component('LibraryInfographicGallery', LibraryInfographicGallery)
   }
